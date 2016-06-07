fad5aa70e9cb8344bb3f95ce82f42899ff86f931
robert.storlind <robert.storlind@sonymobile.com>
2016-06-07 22:59:32 +0200

    Basic code template

---
+++ src/main.js
@@ -0,0 +1,31 @@
+// Create our 'main' state that will contain the game
+/*global Phaser */
+/*jslint node: true */
+'use strict';
+
+var game;
+var mainState = {
+    preload: function () {
+        // This function will be executed at the beginning     
+        // That's where we load the images and sounds 
+    },
+
+    create: function () {
+        // This function is called after the preload function     
+        // Here we set up the game, display sprites, etc.  
+    },
+
+    update: function () {
+        // This function is called 60 times per second    
+        // It contains the game's logic   
+    }
+};
+
+// Initialize Phaser, and create a 400px by 490px game
+game = new Phaser.Game(400, 490);
+
+// Add the 'mainState' and call it 'main'
+game.state.add('main', mainState);
+
+// Start the state to actually start the game
+game.state.start('main');
