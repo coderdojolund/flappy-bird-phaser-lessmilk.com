fc76801d4c0af5d8bfe5d90bb44d529ff3b6ba1f
robert.storlind <robert.storlind@sonymobile.com>
2016-06-09 22:27:44 +0200

    Add death animation

--- src/main.js
+++ src/main.js
@@ -50,14 +50,37 @@ var mainState = {
         if (this.bird.y < 0 || this.bird.y > 490) {
             this.restartGame();
         }
-        game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this);
+        game.physics.arcade.overlap(this.bird, this.pipes, this.hitPipe, null, this);        
         if (this.bird.angle < 20) {
             this.bird.angle += 1;
         }
     },
+
+    hitPipe: function() {
+        // If the bird has already hit a pipe, do nothing
+        // It means the bird is already falling off the screen
+        if (this.bird.alive == false) {
+            return;
+        }
+
+        // Set the alive property of the bird to false
+        this.bird.alive = false;
+
+        // Prevent new pipes from appearing
+        game.time.events.remove(this.timer);
+
+        // Go through all the pipes, and stop their movement
+        this.pipes.forEach(function (p) {
+            p.body.velocity.x = 0;
+        }, this);
+    }, 
     
     // Make the bird jump 
     jump: function () {
+        if (this.bird.alive == false) {
+            return;
+        }
+        
         // Add a vertical velocity to the bird
         this.bird.body.velocity.y = -350;
 
